3410c6c72db2bd89e1f7f9fd700723dcb72171a4
Mayuree <Mayuree@Mayurees-MacBook-Air.local>
2016-12-04 15:35:45 +0700

    Initial Commit

---
+++ NextStop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NextStop
+//
+//  Created by Mayuree on 12/4/16.
+//  Copyright © 2016 it56070048.it.kmitl.ac.th. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
